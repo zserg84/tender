291b1c0d5d2eb14d9162c1dcaf71ea1320108b39
Сергей <sz@smartarenda.ru>
2015-11-05 11:11:43 +0500

    меню

--- modules/site/views/frontend/about/index.php
+++ modules/site/views/frontend/about/index.php
@@ -1,9 +1,8 @@
-<div class="centerbar-wrapper">
-    <div class="ad"><a href="#">Реклама</a></div>
+<div class="ad"><a href="#">Реклама</a></div>
 
-    <div class="content-block">
+<div class="content-block">
+    <?if($model):?>
         <h1><?=$model->title?></h1>
-        <?=$model->text?>
-    </div>
-
+        <div><?=$model->text?></div>
+    <?endif?>
 </div>
--- modules/site/views/frontend/education/index.php
+++ modules/site/views/frontend/education/index.php
@@ -1,22 +1,20 @@
 <?
 use yii\widgets\ListView;
 ?>
-<div class="centerbar-wrapper">
-    <div class="ad"><a href="#">Реклама</a></div>
+<div class="ad"><a href="#">Реклама</a></div>
 
-    <div class="content-block">
-        <h1>Обучение</h1>
-    </div>
-
-    <table class="orders">
-        <tbody>
-        <?
-        echo ListView::widget([
-            'dataProvider' => $dataProvider,
-            'itemView' => '_item',
-            'summary' => false,
-        ]);
-        ?>
-        </tbody>
-    </table>
+<div class="content-block">
+    <h1>Обучение</h1>
 </div>
+
+<table class="orders">
+    <tbody>
+    <?
+    echo ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemView' => '_item',
+        'summary' => false,
+    ]);
+    ?>
+    </tbody>
+</table>
